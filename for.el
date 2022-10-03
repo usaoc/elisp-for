e82e63f5235d22901c4774eef3ff3d17df6472b7
Wing Hei Chan <whmunkchan@outlook.com>
2022-10-04 05:43:27 +0800

    ; Bump version to 1.3
    - New iteration macro `for-foldr'/`for-iter' (c0c3505);
    - new sequence constructor `for-in-infinite-range' (0eb13bf);
    - fix flonum-zero case of `for-vector'/`for-string' (938cc7b);
    - fix wrong semantics of `for-in-naturals' (7be9923);
    - apply alpha conversino to loop bindings (04dd58d);
    - miscellaneous changes.
    
    * for.el:
    * for.texi:
    Version bump.

--- for.el
+++ for.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/usaoc/elisp-for
 ;; Keywords: extensions
 ;; Package-Requires: ((emacs "28.1"))
-;; Version: 1.2
+;; Version: 1.3
 
 ;; This file is not part of GNU Emacs.
 
